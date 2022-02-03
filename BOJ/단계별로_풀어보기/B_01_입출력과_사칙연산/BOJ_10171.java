30075397ba4e582876f3278c4c37812a89e00c28
KimSu040973 <50227432+KimSu040973@users.noreply.github.com>
2022-02-03 18:12:51 +0900

    Create BOJ_10171.java

---
+++ BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_10171.java
@@ -0,0 +1,14 @@
+/**
+* 고양이를 출력한다.
+* 참고로 자바에서 몇몇 문자는 단독으로 써서 출력 X ( EX> 백슬래시 와 큰 따옴표 )
+* 문자를 출력하기 위해 조합을 만들어서 출력할 수 있도록 하는데 이를 Escape Sequance 라고 한다.
+* 이스케이프 시퀀스는 백슬래시(\) + 문자 의 조합으로 쓰인다.
+*/
+public class Main{
+    public static void main(String[] a){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
