7f5f25ba662df74fe5382df42ce71968ffb29fe7
KimSu040973 <50227432+KimSu040973@users.noreply.github.com>
2022-02-03 17:56:08 +0900

    Update BOJ_2557.java

--- BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_2557.java
+++ BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_2557.java
@@ -1,6 +1,9 @@
 /**
-* ( 참고로 백준 알고리즘에서는 클래스명을 Main으로 해주어야 한다 )
+*
+*백준 알고리즘에서는 클래스명을 Main으로 해주어야 한다 
+*
 */
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
