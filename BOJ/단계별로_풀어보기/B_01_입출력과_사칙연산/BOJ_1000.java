5e9d0ebbca7e3c3c0e6619d9e46e9a2c6c99ca6b
KimSu040973 <50227432+KimSu040973@users.noreply.github.com>
2022-02-03 19:09:47 +0900

    Update BOJ_1000.java

--- BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_1000.java
+++ BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_1000.java
@@ -18,27 +18,30 @@ public class Main{
 
 
 // WAY2 메모리 14188kb  시간	124ms
+// BufferedReader 의 경우 문자열을 받는 대표적인 방법은 readLine() 과 read() 이다.
 import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-    //문자 하나만 입력 받기 (하나 이상은 readline() 사용할것)
-    //	int a = br.read() 로 2를 눌러 입력 받음 50 임 그래서 '0'인 48을 빼야 50-48=2가 나옴
-    int a = br.read() - '0'; 
+   	     //read() 메소드는 문자 1 개만 아스키코드(문자)로 읽어 정수형태로 반환
+ 	     //만약 2라는 정수를 입력해도 문자로 인식하여 실제 변수엔 아스키 코드 50이 저장된다 
+	     //그래서 '0'인 48을 빼야 50-48=2가 나온다
+	     int a = br.read() - '0'; 
+	     //(공백 구분)스페이스나 엔터 받아서 
+	     br.read();
     
-		//(공백 구분)스페이스나 엔터 받아서 
-    br.read();
-    
-		//다음 문자 입력 받기
-    int b = br.read() - '0';    
+ 	     //다음 문자 입력 받기
+             int b = br.read() - '0';    
 		
-		System.out.println(a + b);
+	     System.out.println(a + b);
 	}
 }
 
 
 // WAY3 메모리 14184kb  시간	124ms
+// 단순 규칙으로 문자열을 분리를 위해  StringTokenizer( "문자열" , 구분자 ); 
+// 구분된 변수를 꺼낼 때는 차례대로 nextToken(); 을 해주면 문자열을 반환
 import java.io.*;
 import java.util.StringTokenizer;
  
@@ -48,7 +51,7 @@ public class Main {
  
  		String str = br.readLine(); //한번에 문자로 입력
 		StringTokenizer st = new StringTokenizer(str," "); //입력받은 문자 스페이스로 구분
-		int a = Integer.parseInt(st.nextToken()); //단순 규칙으로 문자열을 분리한 것을 정수 자료형으로 초기화
+		int a = Integer.parseInt(st.nextToken()); // 정수 자료형으로 초기화
 		int b = Integer.parseInt(st.nextToken());
 		
 		System.out.println(a+b);
