af9bc5fb740798cdeb961205424ec81d203cee77
KimSu040973 <50227432+KimSu040973@users.noreply.github.com>
2022-02-04 16:33:27 +0900

    Create BOJ_10430.java

---
+++ BOJ/단계별로_풀어보기/B_01_입출력과_사칙연산/BOJ_10430.java
@@ -0,0 +1,62 @@
+/**
+
+입력
+첫째 줄에 A, B, C가 순서대로 주어진다. (2 ≤ A, B, C ≤ 10000)
+
+출력
+첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+*/
+
+// WAY1 메모리 17720kb 시간 216ms
+// Scanner 를 사용하여 in.nextInt() 로 정수 입력받는 방법
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int A, B, C;
+        A = s.nextInt();
+        B = s.nextInt();
+        C = s.nextInt();
+        
+        s.close(); //리소스 누수로 사용 적극권장,,
+        
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+    }
+}
+
+
+// WAY2 메모리 14312kb 시간 124ms
+// BufferedReader 을 쓰는 방식
+// StringTokenizer 클래스를 이용하여 분리해주는 방법
+// 단순 규칙으로 문자열을 분리를 위해  StringTokenizer( "문자열" , 구분자 ); **구분자: " " or "\n"
+// 구분된 변수를 꺼낼 때는 차례대로 nextToken(); 을 해주면 문자열을 반환
+// 반환된 문자열을 Integer.parseInt()로 int 형으로 변환
+
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.print(((A%C) * (B%C))%C);
+    }
+}
+
+
+
+
+
