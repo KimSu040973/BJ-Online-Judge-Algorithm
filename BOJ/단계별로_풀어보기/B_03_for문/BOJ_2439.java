39eb4d0c394f042a242f01329932022bdf3e70ab
KimSu040973 <50227432+KimSu040973@users.noreply.github.com>
2022-02-10 16:50:51 +0900

    Update BOJ_2439.java

--- BOJ/단계별로_풀어보기/B_03_for문/BOJ_2439.java
+++ BOJ/단계별로_풀어보기/B_03_for문/BOJ_2439.java
@@ -1,1 +1,17 @@
 
+import java.util.*;
+class Main{
+    public static void main(String[] args)  {
+        Scanner s = new Scanner(System.in); 
+        int N =s.nextInt();
+        s.close(); //리소스 누수,,
+        for(int i = 1 ; i <= N ; i++){
+	        for(int j = N ; j > 0 ; j--){
+                if(j > i) System.out.print(" ");
+                else System.out.print("*");
+            } 
+          
+            System.out.println();
+        }
+    }
+}
